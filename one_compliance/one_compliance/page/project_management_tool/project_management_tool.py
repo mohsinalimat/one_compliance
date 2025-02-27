fd1d2017f250ecc4731a5d6dca002ca96312e534
Nidhashirinniyas <nidhashirin34@gmail.com>
2025-02-27 17:04:25 +0530

    feat : Project will be visible if user has a open task

--- one_compliance/one_compliance/page/project_management_tool/project_management_tool.py
+++ one_compliance/one_compliance/page/project_management_tool/project_management_tool.py
@@ -2,52 +2,58 @@ import frappe
 from frappe.utils import get_datetime
 
 @frappe.whitelist()
-def get_project(status = None, project = None, customer = None, department = None, sub_category = None, employee = None, from_date = None, to_date = None):
-    user_id = f'"{employee}"' if id else None
-    # Construct the SQL query to fetch list of projects
+def get_project(status=None, project=None, customer=None, department=None, sub_category=None, employee=None, from_date=None, to_date=None):
+    user_id = f'"{employee}"' if employee else None
+
+    # Construct SQL query to fetch list of projects that have at least one task with 'Open' status
     query = """
-	SELECT
-        name, project_name, customer, department, compliance_sub_category, _assign, expected_start_date, expected_end_date, status
+    SELECT DISTINCT
+        p.name, p.project_name, p.customer, p.department, p.compliance_sub_category, p._assign,
+        p.expected_start_date, p.expected_end_date, p.status
     FROM
-        tabProject
+        tabProject p
+    INNER JOIN
+        tabTask t ON t.project = p.name
+    WHERE
+        t.status = 'Open'
     """
 
     if status:
-            query += f" WHERE status = '{status}'"
+        query += f" AND p.status = '{status}'"
     else:
-        query += " WHERE status IN ('open', 'working', 'overdue')"
+        query += " AND p.status IN ('Open', 'Working', 'Overdue')"
 
     if project:
-            query += f" AND name = '{project}'"
+        query += f" AND p.name = '{project}'"
 
     if customer:
-            query += f" AND customer = '{customer}'"
+        query += f" AND p.customer = '{customer}'"
 
     if department:
-            query += f" AND department = '{department}'"
+        query += f" AND p.department = '{department}'"
 
     if sub_category:
-            query += f" AND compliance_sub_category = '{sub_category}'"
+        query += f" AND p.compliance_sub_category = '{sub_category}'"
 
     if employee:
-            query += f" AND _assign LIKE '%{user_id}%'"
+        query += f" AND t._assign LIKE '%{user_id}%'"
 
     if from_date:
-            query += f" AND expected_start_date >= '{from_date}'"
+        query += f" AND p.expected_start_date >= '{from_date}'"
 
     if to_date:
-            query += f" AND expected_end_date < '{to_date}'"
+        query += f" AND p.expected_end_date < '{to_date}'"
 
-    query += """ ORDER BY
-            modified DESC;"""
+    query += " ORDER BY p.modified DESC;"
 
     project_list = frappe.db.sql(query, as_dict=1)
+    # Process employee assignment details
     for project in project_list:
         project['employee_names'] = []
         if project['_assign']:
             user_ids = frappe.parse_json(project['_assign'])
             if user_ids:
-                # Query to get the employee name from id
+                # Query to get employee names
                 user_names_query = """
                     SELECT name, employee_name, user_id FROM `tabEmployee`
                     WHERE user_id IN ({})
