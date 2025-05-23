33b2a2d15d784999395ec1187202b77592cd6fa0
Neha Fathima <nehaputhur443@gmail.com>
2025-05-23 13:12:47 +0530

    feat: created a report to list the  compliance agreement which has missing linked projects

---
+++ one_compliance/one_compliance/report/project_compliance__agreement_link_report/project_compliance__agreement_link_report.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2025, efeone and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Project Compliance  Agreement Link Report"] = {
+	"filters": [
+		{
+            "fieldname": "customer",
+            "label": __("Client"),
+            "fieldtype": "Link",
+            "options": "Customer",
+        },
+        {
+            "fieldname": "compliance_sub_category",
+            "label": __("Compliance Sub Category"),
+            "fieldtype": "Link",
+            "options": "Compliance Sub Category",
+        },
+        {
+            "fieldname": "date_basis",
+            "label": "Date Basis",
+            "fieldtype": "Select",
+            "options": ["Posting Date","Valid Upto","Valid From"]
+        },
+        {
+            "fieldname": "from",
+            "label": __("From"),
+            "fieldtype": "Date"
+        },
+        {
+            "fieldname": "to",
+            "label": __("To"),
+            "fieldtype": "Date"
+        },
+		{
+            "fieldname": "include_expired",
+            "label": __("Include Expired"),
+            "fieldtype": "Check"
+        }
+	]
+};
